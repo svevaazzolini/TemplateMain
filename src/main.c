bdad44ed0425c89d9565d7658ebc0d1f99113967
svevaazzolini <sveva.azzolini@gmail.com>
2019-03-25 21:04:26 +0000

    Apertura e scrittura in file

--- src/main.c
+++ src/main.c
@@ -11,20 +11,25 @@
 int main(int argc, char *argv[]) 
 {
 	FILE *id_file;
-
+        char file_name[20];
 	#ifdef __LINUX__
 	printf("%s è un programma scritto in Linux" EOL, argv[0]);
 	#endif
 	
 	TRACE("Questo è l'inizio del programma"EOL);
 
-	id_file=fopen("pippo.txt", "r");
+	sprintf(&file_name[0],"pippo.txt");
+	id_file=fopen(&file_name[0], "r+");
 	if(id_file==NULL){
-		printf("Errore %d nell'apertura del file", errno);
+		printf("Errore %d nell'apertura del file %s" EOL, errno,&file_name[0]);
 		if(errno==ENOENT){
 			printf("File inesistente"EOL);
 		}
 	}
+	if(id_file!=NULL){
+		fprintf(id_file, "Ciao questo è il contenuto del file" EOL);
+	}
+
 
 	if(id_file!=NULL){
 		fclose(id_file);
